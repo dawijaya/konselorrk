2eff52c0625b3ff98235378e249020b6d311891b
dawijaya <113819824+dawijaya@users.noreply.github.com>
2023-04-11 17:26:49 +0700

    Add files via upload

---
+++ panduan.js
@@ -0,0 +1,6 @@
+const consultButton = document.querySelector('.consult-button');
+
+consultButton.addEventListener('click', function() {
+    // Ganti URL pada line di bawah ini dengan URL halaman konsultasi Anda
+    window.location.href = 'https://wa.me/6282246402832';
+});
