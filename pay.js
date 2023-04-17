bc41fbbfa22355617e04fb4735deeedc1f971e47
dawijaya <113819824+dawijaya@users.noreply.github.com>
2023-04-18 06:52:31 +0700

    Update pay.js

--- pay.js
+++ pay.js
@@ -2,5 +2,5 @@ const payButton = document.querySelector('.pay-button');
 
 payButton.addEventListener('click', function() {
     // Ganti URL pada line di bawah ini dengan URL halaman pembayaran Anda
-    window.location.href = 'https://wa.me/6282246402832';
+    window.location.href = 'mailto:Skomariah295@gmail.com';
 });
